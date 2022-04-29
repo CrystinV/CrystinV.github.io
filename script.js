550b5790972413caf35ffc0847a49da47c7890ad
CrystinV <crystinvazquez@gmail.com>
2022-04-29 14:25:37 -0400

    Added Breakout project button

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleDark(){
 	var home = document.getElementById("home");
 	home.classList.toggle("dark-mode");
 }
+
+// Stop slider/contact info when it gets above the footer
+$(document).scroll(function() {
+    checkOffset();
+});
+
+function checkOffset() {
+    if($('.footer-float').offset().top + $('.footer-float').height() 
+                                           >= $('#footer').offset().top + 5)
+        $('.slider').css('display', 'none'),
+		$('.contact .btn').css('display', 'none');
+    if($(document).scrollTop() + window.innerHeight < $('#footer').offset().top + 15)
+		$('.slider').css('display', 'inline-block'),
+		$('.contact .btn').css('display', 'inline-block');
+}
